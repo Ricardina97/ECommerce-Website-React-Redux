328a668d25d2cd4fcc7f479b0eeb09ecde144ba5
Vera Dias <veradias97@hotmail.com>
2020-09-12 23:20:37 +0100

    refactor code

--- crwn-clothing/src/components/collections-overview/collections-overview.component.jsx
+++ crwn-clothing/src/components/collections-overview/collections-overview.component.jsx
@@ -2,21 +2,24 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 import CollectionPreview from '../collection-preview/collection-preview.component';
+
+import { selectCollections } from '../../redux/shop/shop.selectors';
 
 import './collections-overview.styles.scss';
 
-const CollectionsOverview = ({ collections }) => (
-    <div className='collections-overview'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-    </div>
-);
+const CollectionsOverview = ({ collections }) => {
+    return (
+        <div className='collections-overview'>
+            {collections.map(({ id, ...otherCollectionProps }) => (
+                <CollectionPreview key={id} {...otherCollectionProps} />
+            ))}
+        </div>
+    );
+};
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollectionsForPreview,
+    collections: selectCollections,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
