118dc61364d819ddbf55ea6d1d3ab659b9020f73
Vera Dias <veradias97@hotmail.com>
2020-09-19 15:44:41 +0100

    finished project

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import { Component } from 'react';
-import './App.css';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-sign-up.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
+
 import Header from './components/header/header.component';
-import {
-    auth,
-    createUserProfileDocument,
-    addCollectionAndDocuments,
-} from './firebase/firebase.utils';
+
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
-import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
-class App extends Component {
+
+class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        const { setCurrentUser, collectionsArray } = this.props;
+        const { setCurrentUser } = this.props;
+
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
             if (userAuth) {
                 const userRef = await createUserProfileDocument(userAuth);
@@ -34,11 +34,8 @@ class App extends Component {
                     });
                 });
             }
+
             setCurrentUser(userAuth);
-            // addCollectionAndDocuments(
-            //     'collections',
-            //     collectionsArray.map(({ title, items }) => ({ title, items }))
-            // );
         });
     }
 
@@ -70,11 +67,13 @@ class App extends Component {
         );
     }
 }
+
 const mapStateToProps = createStructuredSelector({
     currentUser: selectCurrentUser,
-    collectionsArray: selectCollectionsForPreview,
 });
+
 const mapDispatchToProps = (dispatch) => ({
     setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
+
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- crwn-clothing/src/redux/shop/shop.selectors.js
+++ crwn-clothing/src/redux/shop/shop.selectors.js
@@ -14,12 +14,12 @@ export const selectCollectionsForPreview = createSelector(
             ? Object.keys(collections).map((key) => collections[key])
             : []
 );
+
 export const selectCollection = (collectionUrlParam) =>
-    createSelector([selectCollections], (collections) => {
-        return collections.find((collection) => {
-            return collection.routeName === collectionUrlParam;
-        });
-    });
+    createSelector([selectCollections], (collections) =>
+        collections ? collections[collectionUrlParam] : null
+    );
+
 export const selectIsCollectionFetching = createSelector(
     [selectShop],
     (shop) => shop.isFetching
